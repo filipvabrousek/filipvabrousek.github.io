c6c56a0d662cecb19da82063810655e3e4e9891b
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-12-10 14:34:37 +0100

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,33 @@
+var els = document.querySelectorAll(".hidden");
+var height = window.innerHeight;
+
+function addEL() {
+    window.addEventListener("load", () => {
+        els.forEach((el, i) => {
+            var top = el.getBoundingClientRect().top;
+
+            if (top - height <= 0) {
+                els[i].className = els[i].className.replace("hidden", "fade-in");
+            }
+        });
+    });
+}
+        
+        
+function addPEL() {
+    window.addEventListener("scroll", () => {
+        els.forEach((el, i) => {
+            var top = el.getBoundingClientRect().top;
+
+            if (top - height <= 0) {
+                els[i].className = els[i].className.replace("hidden", "fade-in");
+            }
+        });
+    });
+}
+
+
+addEL();
+addPEL();
+       
+
