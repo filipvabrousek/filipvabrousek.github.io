4cef9d470be301ef1df6e0d7e66c0563f22843ca
Filip Vabrousek <filipvabrousek1@gmail.com>
2025-09-27 17:03:33 +0200

    Add files via upload

---
+++ sls-beta/components/blue70Button.js
@@ -0,0 +1,22 @@
+class Blue70Button extends Base {
+  constructor(obj){
+    super();
+    this.obj = obj;
+  }
+
+  render(){
+    return new Link().set({
+                text: this.obj.title,
+                background: "#3498db",
+                color: "white",
+                url: this.obj.url, //`https://filipvabrousek.github.io/sls-beta/detail.html?id=${this.obj.id}`, //`./detail.html?id=${this.obj.id}`,
+                fixMobileTap: true,
+                hover: {
+                  color: "#3498db",
+                  background: "white"
+                },
+                radius: "1rem",
+                pad: [{lr: "2rem"}, {tb: "1rem"}]
+              })
+  }
+}
